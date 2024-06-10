3823ab185a923aaedb30e6e719db63b13159c339
Balajitrader <balajisecurities000@gmail.com>
2024-06-10 14:35:45 +0530

    env file changes done

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,5 +1,5 @@
-const backendDomin = "http://localhost:8080"
-
+const backendDomin = process.env.BACKEND_URL;
+//https://kaustubhstore.onrender.com
 const SummaryApi = {
     signUP : {
         url : `${backendDomin}/api/signup`,
